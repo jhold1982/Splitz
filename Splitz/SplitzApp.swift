3431ddc4ebdc5de9cd18cb74ba68a67d43e5eb6e
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-16 20:18:19 -0600

    Initial Commit

---
+++ Splitz/SplitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitzApp.swift
+//  Splitz
+//
+//  Created by Justin Hold on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
