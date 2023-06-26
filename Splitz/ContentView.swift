6a1f695d501221c5f7031424e956a2457fa6a22b
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-26 14:50:44 -0500

    Refactored code

--- Splitz/ContentView.swift
+++ Splitz/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
 	
+	// MARK: - PROPERTIES
 	@State private var checkAmount = 0.0
 	@State private var numberOfPeople = 2
 	@State private var tipPercentage = 20
@@ -16,7 +17,6 @@ struct ContentView: View {
 	@FocusState private var amountIsFocused: Bool
 	
 	let tipPercentages = [10, 15, 18, 20, 75, 100, 0]
-	
 	let localCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(
 		code: Locale.current.currency?.identifier ?? "USD"
 	)
@@ -26,19 +26,15 @@ struct ContentView: View {
 		let tipValue = checkAmount / 100 * tipSelection
 		return checkAmount + tipValue
 	}
-	
 	var totalPerPerson: Double {
 		grandTotal / Double(numberOfPeople + 2)
 	}
 	
+	// MARK: - BODY
 	var body: some View {
-		
 		ZStack {
-			
 			NavigationStack {
-				
 				Form {
-					
 					Section("Check Amount & Number of People") {
 						TextField("Check Amount", value: $checkAmount, format: localCurrency)
 							.keyboardType(.decimalPad)
@@ -46,35 +42,26 @@ struct ContentView: View {
 						Picker("Number of people", selection: $numberOfPeople) {
 							ForEach(2..<100, id: \.self) {
 								Text("\($0) people")
-							}
-						}
-					}
-					
-					
+							} //: END OF FOR EACH
+						} //: END OF PICKER
+					} //: END OF SECTION
 					Section("Good Karma") {
 						Picker("Tip Percent", selection: $tipPercentage) {
 							ForEach(tipPercentages, id: \.self) {
 								Text($0, format: .percent)
-							}
-						}
+							} //: END OF FOR EACH
+						} //: END OF PICKER
 						.pickerStyle(MenuPickerStyle())
-					}
-					
-					
+					} //: END OF SECTION
 					Section("Total Amount") {
 						Text(grandTotal, format: localCurrency)
 							.foregroundColor(tipPercentage == 0 ? .red : .primary)
-					}
-					
-					
+					} //: END OF SECTION
 					Section("Amount Per Person") {
 						Text(totalPerPerson, format: localCurrency)
-					}
-				}
-				
+					} //: END OF SECTION
+				} //: END OF FORM
 				.navigationTitle("Splitz")
-				
-				// MARK: DONE KEYBOARD BUTTON
 				.toolbar {
 					ToolbarItemGroup(placement: .keyboard) {
 						Spacer()
@@ -84,7 +71,6 @@ struct ContentView: View {
 						.padding()
 					}
 				}
-				// MARK: TRASH ICON, RESET ALL INPUT/OUTPUT
 				.toolbar {
 					Button {
 						reset()
@@ -93,18 +79,19 @@ struct ContentView: View {
 					}
 					.padding()
 				}
-			}
-		}
-	}
+			} //: END OF NAVIGATION STACK
+		} //: END OF ZSTACK
+	} //: END OF BODY
 	
+	// MARK: - FUNCTIONS
 	func reset() {
 		checkAmount = 0.0
 		numberOfPeople = 2
 		tipPercentage = 20
 	}
-	
 }
 
+// MARK: - PREVIEWS
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
